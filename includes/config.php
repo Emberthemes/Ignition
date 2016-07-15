89341184e64ea39a63b8193e9b88446e96a7fa9d
Dean <hello@shadyx.com>
2016-07-16 00:45:57 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -9,4 +9,14 @@
 $config['title'] = 'This is the title!';
 $config['description'] = 'This is the description!';
 
+
+/**
+ * Database Infomation
+ */
+
+$db['host'] = 'localhost';
+$db['name'] = 'database name';
+$db['user'] = 'database user';
+$db['pass'] = 'database password';
+
 ?>
