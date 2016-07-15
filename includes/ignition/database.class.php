6a672790a70cbc3c73de1681ab6ac1ca1c765c44
Dean <hello@shadyx.com>
2016-07-16 00:30:21 +0100

    Create database.class.php

---
+++ includes/ignition/database.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/** 
+ * Database Interaction Class
+ * A barebones database class for ignition 
+ * Emberthemes.net
+ */
+
+Class Database {
+	
+    private $dbHost;
+    private $dbName;
+    private $dbUser;
+    private $dbPass;
+
+function __construct($db) {
+
+	$this->dbHost = $db['host'];
+	$this->dbName = $db['name'];
+	$this->dbUser = $db['user'];
+	$this->dbPass = $db['pass'];
+
+	$connect = new mysqli($this->dbHost, $this->dbUser, $this->dbPass, $this->dbName);
+	if($connect->connect_errno > 0){
+		die('Database Error: [' . $connect->connect_error . ']');
+	}
+}
+
+
+}
+
+?>
