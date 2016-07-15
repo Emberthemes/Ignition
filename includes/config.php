63b895ed1c60093a2bc60f0d821896b78f8abc1d
Dean <hello@shadyx.com>
2016-07-16 00:43:36 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,12 @@
+<?php
 
+/**
+ * Config File
+ * All the websites configuration options are here
+ * Emberthemes.net
+ */
+
+$config['title'] = 'This is the title!';
+$config['description'] = 'This is the description!';
+
+?>
