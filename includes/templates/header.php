a2dfc068c8338bd10e1fdfa8fe754b3e653585f7
Dean <hello@shadyx.com>
2016-07-16 02:08:31 +0100

    Update header.php

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<? echo $config['description']; ?>">
+    <link rel="icon" href="{todo}">
+    <title><? echo $config['title']; ?></title>
+    <!-- CSS -->
+    <link href="{todo}" rel="stylesheet">
+  </head>
+  <body>
