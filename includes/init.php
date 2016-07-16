45104d4be5535bc910792f1b57b5e54b93c21c61
Dean <hello@shadyx.com>
2016-07-16 01:50:37 +0100

    Update init.php

--- includes/init.php
+++ includes/init.php
@@ -6,4 +6,23 @@
  */
 
 
+// Define Paths
+
+define('PATH_ABSOLUTE', dirname(__FILE__) . '/');
+define('PATH_LANGUAGE', PATH_ABSOLUTE . 'language/');
+
+include (PATH_ABSOLUTE . 'config.php');
+
+// Check if language file exists, default to engrish
+
+if (file_exists(PATH_LANGUAGE . $config['language'] . '.php')) {
+  
+	include (PATH_LANGUAGE . $config['language'] . '.php');
+	
+} else {
+  
+	include (PATH_LANGUAGE . 'en.php');
+	
+}
+
 ?>
